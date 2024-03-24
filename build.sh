ec55789f4007d40689a689f18dab778ad4eb48b3
Alex Renoki <alex@renoki.org>
2024-03-25 01:02:30 +0200

    Use npx

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pnpx sponsorkit --dir=./assets -w=800
-pnpx sponsorkit --dir=./assets -w=1800 --name=sponsors.wide
-pnpx sponsorkit --dir=./assets -w=800 --name=sponsors.part1 --filter=">=9.9"
-pnpx sponsorkit --dir=./assets -w=800 --name=sponsors.part2 --filter="<9.9"
+npx sponsorkit --dir=./assets -w=800
+npx sponsorkit --dir=./assets -w=1800 --name=sponsors.wide
+npx sponsorkit --dir=./assets -w=800 --name=sponsors.part1 --filter=">=9.9"
+npx sponsorkit --dir=./assets -w=800 --name=sponsors.part2 --filter="<9.9"
